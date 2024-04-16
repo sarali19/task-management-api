9e31a354a2bbe78fa0e78f72d076ce2a6dcb2c76
Oussama Abdoune <abdouneoussama592@gmail.com>
2024-04-16 23:34:43 +0100

    implementing the CRUD operations for User

--- src/main/java/org/application/repositories/UserRepository.java
+++ src/main/java/org/application/repositories/UserRepository.java
@@ -1,4 +1,49 @@
 package org.application.repositories;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.application.entities.User;
+
+import java.util.List;
+import java.util.Optional;
+
+
 public class UserRepository {
+
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("task-management-unit");
+    private final EntityManager em = emf.createEntityManager();
+
+    public List<User> getAllUsers() {
+        List<User> users = em.createQuery("select u from User u ", User.class).getResultList();
+        return users;
+    }
+
+    public User createUser(User user) {
+        em.getTransaction().begin();
+        em.persist(user);
+        em.getTransaction().commit();
+        return user;
+    }
+
+    public Optional<User> findUserById(Long id) {
+        return Optional.ofNullable(em.find(User.class, id));
+    }
+
+    public void deleteUser(Long id) {
+        User user = findUserById(id).orElseThrow(() -> new IllegalArgumentException("invalid user id : " + id));
+        em.getTransaction().begin();
+        em.remove(user);
+        em.getTransaction().commit();
+    }
+
+
+    public User updateUser(User user) {
+        em.getTransaction().begin();
+        User updatedUser = em.merge(user);
+        em.getTransaction().commit();
+        return updatedUser;
+    }
+
+
 }
--- src/main/java/org/application/rest/UserResource.java
+++ src/main/java/org/application/rest/UserResource.java
@@ -1,7 +1,61 @@
 package org.application.rest;
 
-import jakarta.ws.rs.Path;
+import jakarta.persistence.PersistenceException;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.application.entities.User;
+import org.application.repositories.UserRepository;
+
+import java.util.List;
 
 @Path("/users")
 public class UserResource {
+    private final UserRepository userRepository = new UserRepository();
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User createUser(User user) {
+        try {
+            return userRepository.createUser(user);
+        } catch (PersistenceException persistenceException) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<User> getUsers() {
+        return userRepository.getAllUsers();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public User findUserById(@PathParam("id") Long id) {
+        return userRepository.findUserById(id).orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteUserById(@PathParam("id") Long id) {
+        try {
+            userRepository.deleteUser(id);
+        } catch (IllegalArgumentException exception) {
+            System.out.println("cannot delete the user with the id of : " + id);
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User updateUser(User user) {
+        try {
+            return userRepository.updateUser(user);
+        } catch (PersistenceException exception) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+    }
 }
