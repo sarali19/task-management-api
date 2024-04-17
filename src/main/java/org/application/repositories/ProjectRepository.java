7cbe53d2cca17a5f1c61d9e145c04451d391d259
Oussama Abdoune <abdouneoussama592@gmail.com>
2024-04-17 08:12:55 +0100

    implementing the CRUD operations for PROJECT

--- src/main/java/org/application/repositories/ProjectRepository.java
+++ src/main/java/org/application/repositories/ProjectRepository.java
@@ -1,4 +1,47 @@
 package org.application.repositories;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.ws.rs.WebApplicationException;
+import org.application.entities.Project;
+
+import java.awt.print.PrinterJob;
+import java.util.List;
+import java.util.Optional;
+
 public class ProjectRepository {
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("task-management-unit");
+    private final EntityManager em = emf.createEntityManager();
+
+    public List<Project> getAllProjects() {
+        List<Project> projects = em.createQuery("select p from Project p ",Project.class).getResultList() ;
+        return projects ;
+    }
+
+    public Project createProject(Project project) {
+        em.getTransaction().begin();
+        em.persist(project);
+        em.getTransaction().commit();
+        return project ;
+    }
+
+    public Optional<Project> findProjectById(Long id) {
+        return Optional.ofNullable(em.find(Project.class, id));
+    }
+
+    public void deleteProject(Long id) {
+        Project project = findProjectById(id).
+                orElseThrow(() -> new IllegalArgumentException("cannot find a user with the id of : " + id));
+        em.getTransaction().begin();
+        em.remove(project);
+        em.getTransaction().commit();
+    }
+
+    public Project updateProject(Project project) {
+        em.getTransaction().begin();
+        Project updatedProject = em.merge(project) ;
+        em.getTransaction().commit();
+        return updatedProject ;
+    }
 }
--- src/main/java/org/application/rest/ProjectResource.java
+++ src/main/java/org/application/rest/ProjectResource.java
@@ -1,8 +1,63 @@
 package org.application.rest;
 
-import jakarta.ws.rs.Path;
+import jakarta.persistence.PersistenceException;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.application.entities.Project;
+import org.application.entities.User;
+import org.application.repositories.ProjectRepository;
+
+import java.util.List;
 
 @Path("/projects")
 public class ProjectResource {
+    private final ProjectRepository projectRepository = new ProjectRepository() ;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Project> getAllProjects() {
+        return projectRepository.getAllProjects() ;
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Project findProjectById(@PathParam("id") Long id) {
+        return projectRepository.findProjectById(id).orElseThrow(() -> new WebApplicationException(Response.Status.NOT_FOUND));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Project createProject(Project project) {
+        try {
+            return projectRepository.createProject(project);
+        } catch (PersistenceException persistenceException) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteProjectById(@PathParam("id") Long id ){
+        try {
+            projectRepository.deleteProject(id);
+        } catch (IllegalArgumentException exception) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND) ;
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Project updateProject(Project project) {
+        try {
+            return projectRepository.updateProject(project);
+        } catch (PersistenceException exception) {
+            throw new WebApplicationException(Response.Status.BAD_REQUEST);
+        }
+    }
+
 
 }
